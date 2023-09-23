f75cbb6514e93cf8281fad8e28ff7104bf7845f2
Gaetano <gaetanobarreca@gmail.com>
2023-09-22 21:27:50 -0700

    Working on the Getting Creative Lab assigment

---
+++ src/main/java/com/example/GetCreativeLab.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/*  Create a small program in the next five minutes that takes user input 
+    using a scanner and any data type. Display the value input. Then, perform some 
+    type of manipulation on that value and display that result. Please use ALL data types and ALL operators.
+*/
+public class GetCreativeLab {
+    public static void main(String[] args) {
+
+        // starting code
+        
+    }
+}
